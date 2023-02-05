31bd60f094af1dba72940662b0bb48972bf27dab
Mark McFate <summitt.dweller@gmail.com>
2023-02-05 10:42:45 -0600

    Completed make-markdown.py

--- make-markdown.py
+++ make-markdown.py
@@ -1,5 +1,21 @@
 import argparse
 import os
+from datetime import datetime
+
+front_matter = (
+"---\n" + 
+"title: TITLE\n" + 
+"publishDate: DATE\n" +
+"last_modified_at: 2023-01-01T00:00:01\n" + 
+"draft: false\n" +
+"description: DESCRIPTION\n" +
+"tags:\n" + 
+"  - TAG_1\n" +
+"  - TAG_2\n" +
+"azure:\n" + 
+"  dir: DIR\n" +
+"  subdir: SUB\n" +  
+"---\n\n" )
 
 ## Function definitions
 
@@ -25,6 +41,19 @@ def frame_dir_to_markdown(dir, prefix, ext="png"):
     # print(files)
     # print("")
 
+    # Break the `prefix` and `dir` into parent/child parts
+    parent = os.path.dirname(prefix)
+    child = os.path.basename(dir)    
+
+    # Substitute known values into the front matter
+    a = front_matter.replace("DATE", datetime.now().strftime("%Y-%m-%d"))
+    b = a.replace("DIR", parent)
+    c = b.replace("SUB", child)
+
+    # Write the front matter to the new .md file
+    md.write(c) 
+
+    # Add each file's `figure` declaration to the .md file
     for path in files:
         add_file_to_markdown(path, md, prefix)
 
@@ -55,7 +84,7 @@ if args["url"] is None or args["dir"] is None:
     ap.print_help()
 else:
 
-    ext = args["extension"].strip(".")
+    ext = args["ext"].strip(".")
     dir = args["dir"].strip("/")
     url = args["url"].strip("/")
 
@@ -63,3 +92,16 @@ else:
     frame_dir_to_markdown(dir, url, ext)
 
 
+# ---
+# title: Creating Better Documentation
+# publishDate: 2023-02-04T10:36:03-06:00
+# last_modified_at: 2023-02-04T20:33:36
+# draft: false
+# description: A new approach to creating better documentation here.
+# tags:
+#   - documentation
+#   - Azure
+# azure: 
+#   dir: https://sddocs.blob.core.windows.net/documentation/
+#   subdir: Better-Documentation  
+# ---
